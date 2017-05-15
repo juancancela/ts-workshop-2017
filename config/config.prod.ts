9b40286dc1cf681c9c0adde788ff39fd8f63ce34
Juan Cancela <cancela.juancarlos@gmail.com>
2017-05-15 12:48:49 -0300

    Initial commit

---
+++ config/config.prod.ts
@@ -0,0 +1,11 @@
+export default {
+    "postgresql": {
+        "user": "postgres",
+        "database": "postgres",
+        "password": "FFRDFGHT##32__R$$R$%SSDDSSD)()()()Ss@@@__",
+        "host": "35.186.174.249",
+        "port": 5432,
+        "max": 10,
+        "idleTimeoutMillis": 30000
+    }
+}
